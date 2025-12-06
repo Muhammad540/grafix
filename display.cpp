a05b55b4ab15bffb8511a046600e15a5ca9610e2
Muhammad540 <thisisdeahmed@gmail.com>
2025-12-06 20:02:39 +0100

    use eventloop to manage quit event

--- display.cpp
+++ display.cpp
@@ -7,7 +7,7 @@ const int SCREEN_HEIGHT = 480;
 // surface is like a Image datatype 
 SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
-SDL_Surface* gHelloWorld = NULL;
+SDL_Surface* gXout = NULL;
 
 /* starts up SDL and creates a window*/
 bool init() {
@@ -32,9 +32,9 @@ bool init() {
 bool loadMedia() {
     bool success = true;
     
-    gHelloWorld = SDL_LoadBMP("hello_world.bmp");
-    if (gHelloWorld == NULL) {
-        std::cout << "Unable to load the image " << "hello_world.bmp ! SDL Error: " << SDL_GetError() << std::endl; 
+    gXout = SDL_LoadBMP("x.bmp");
+    if (gXout == NULL) {
+        std::cout << "Unable to load the image " << "x.bmp ! SDL Error: " << SDL_GetError() << std::endl; 
         success = false;
     }
 
@@ -42,8 +42,8 @@ bool loadMedia() {
 }
 
 void close() {
-    SDL_FreeSurface(gHelloWorld);
-    gHelloWorld = NULL;
+    SDL_FreeSurface(gXout);
+    gXout = NULL;
 
     SDL_DestroyWindow(gWindow);
     gWindow = NULL;
@@ -58,19 +58,23 @@ int main(){
         if (!loadMedia()){
             std::cout << "Failed to load media !" << std::endl;
         } else {
-            // update the back buffer
-            SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
-            // pushes the blit to the front 
-            // you only display to the screen after all your blits for the current frame are completed
-            // then you call the update window surface 
-            SDL_UpdateWindowSurface(gWindow);
-            // HACK ! to keep the window up
-            SDL_Event e;
             bool quit = false;
-            while (quit == false){
-                while (SDL_PollEvent(&e)) {
-                    if (e.type == SDL_QUIT) quit = true;
+            SDL_Event event;
+            while (!quit){
+                // SDL_PollEvent returns 0 when the event queue is empty
+                while (SDL_PollEvent(&event) != 0){
+                    if (event.type == SDL_QUIT){
+                        quit = true;
+                    }
                 }
+                /* Double Buffering Logic
+                there are two buffers (back and front)
+                    1. Update the back buffer 
+                    2. Push the blit to the front buffer
+                You should first complete all your blits for the current frame and only then you call the update window surface
+                */
+                SDL_BlitSurface(gXout, NULL, gScreenSurface, NULL);
+                SDL_UpdateWindowSurface(gWindow);
             }
         }
     }
