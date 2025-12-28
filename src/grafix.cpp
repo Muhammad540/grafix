607d14c7c00e8f0811f6afe4e424f885e3dc6e3a
Muhammad540 <thisisdeahmed@gmail.com>
2025-12-28 14:45:42 +0100

    add line drawing algorithm

--- src/grafix.cpp
+++ src/grafix.cpp
@@ -117,3 +117,44 @@ void Grafix::display() {
     }
     Wire.endTransmission();
 }
+
+void Grafix::drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t color) {
+    int16_t steep = abs(y1 - y0) > abs(x1 - x0);
+    if (steep) {
+        int16_t temp;
+        temp = x0; x0 = y0; y0 = temp;
+        temp = x1; x1 = y1; y1 = temp;
+    }
+
+    if (x0 > x1) {
+        int16_t temp;
+        temp = x0; x0 = x1; x1 = temp;
+        temp = y0; y0 = y1; y1 = temp;
+    }
+
+    int16_t dx, dy;
+    dx = x1 - x0;
+    dy = abs(y1 - y0);
+
+    int16_t err = dx / 2;
+    int16_t ystep;
+
+    if (y0 < y1) {
+        ystep = 1;
+    } else {
+        ystep = -1;
+    }
+
+    for (; x0 <= x1; x0++) {
+        if (steep) {
+            drawPixel(y0, x0, color);
+        } else {
+            drawPixel(x0, y0, color);
+        }
+        err -= dy;
+        if (err < 0) {
+            y0 += ystep;
+            err += dx;
+        }
+    }
+}
